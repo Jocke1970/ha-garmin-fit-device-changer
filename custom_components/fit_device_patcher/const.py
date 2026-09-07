b9ea55a6ece9dbc9813b6fcb8301abaa760c316a
Joachim Eriksson <sa0azy@gmail.com>
2026-09-07 18:11:24 +0200

    Add integration constants

---
+++ custom_components/fit_device_patcher/const.py
@@ -0,0 +1,9 @@
+"""Constants for FIT Device Patcher."""
+
+DOMAIN = "fit_device_patcher"
+NAME = "FIT Device Patcher"
+STORAGE_KEY = f"{DOMAIN}.profiles"
+STORAGE_VERSION = 1
+
+DEFAULT_PROFILE_KEY = "fenix_7_pro"
+MAX_FIT_FILE_SIZE = 16 * 1024 * 1024
